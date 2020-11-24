361ddee5be3aabfb1b4c9068059782142a21fa1f
alberto <albertocarrascofernandez@hotmail.com>
2020-11-24 16:54:11 +0100

    modelo + interface

---
+++ carritoCompra/src/app/interface/product.ts
@@ -0,0 +1,9 @@
+export interface Product {
+
+    title?: string;//el ? es que el titulo es opcional , puede estar o no
+    desc: string;
+    price: number;
+    picture: string;
+
+//vinculado con models shop.ts
+}
